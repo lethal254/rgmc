703a150b47a835fbaa392f78406d8012cd5832e1
lethal254 <dopelixy2@gmail.com>
2022-08-12 21:44:44 +0300

    Done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,11 +11,11 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div className='w-full bg-inherit min-h-[20vh] flex px-6 mt-20 relative items-center overflow-hidden bg-black'>
+    <div className='w-full bg-inherit min-h-[20vh] flex flex-col-reverse md:flex-row px-6 mt-20 relative items-center overflow-hidden bg-black'>
       <div className='flex-1 flex flex-col space-y-4 mb-4 lg:p-6'>
         <div className='flex space-x-3  text-sm md:text-base'>
           <HiLocationMarker className='text-orange-300 text-xl' />
-          <p>Trm drive, Nairobi, Kenya</p>
+          <p>TRM Drive, Nairobi, Kenya</p>
         </div>
         <div className='flex space-x-3 items-center text-sm md:text-base'>
           <BsFillTelephoneFill className='text-orange-300 text-xl' />
@@ -26,13 +26,13 @@ const Footer = () => {
           <p>rgmcsafarisandcarrental@gmail.com</p>
         </div>
       </div>
-      <div className='flex-1 flex flex-col items-center mb-4'>
+      <div className='flex-1 flex flex-row md:flex-col items-center mb-6 w-full justify-between md:w-fit '>
         {/* Social media icons */}
         <div className='flex space-x-4 text-2xl'>
           <Link href=''>
             <a
               target='_blank'
-              className='cursor-pointer transist hover:text-orange-300'>
+              className='cursor-pointer transist hover:text-orange-300 '>
               <BsTwitter />
             </a>
           </Link>
@@ -45,9 +45,9 @@ const Footer = () => {
           </Link>
         </div>
         {/* Links */}
-        <div className='flex  mt-4 flex-col md:flex-row md:space-x-4 text-sm md:text-base space-y-3 md:space-y-0'>
+        <div className='flex  mt-4  flex-row   md:space-x-4 text-sm md:text-base  md:space-y-0 '>
           <Link href='/'>
-            <div className='flex space-x-3 items-center cursor-pointer'>
+            <div className='flex space-x-3 items-center cursor-pointer mr-4'>
               <div className='h-4 w-1 bg-orange-300'></div>
               <h2 className='font-semibold'>HOME</h2>
             </div>
